549a641aeb6f813902ed80de49b43561e4292cd1
camilamiwa <miwa.ivano@gmail.com>
2022-03-26 16:07:29 -0300

    Um request de get para pegar dados

--- src/pages/familia/dados.js
+++ src/pages/familia/dados.js
@@ -1,13 +1,50 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+
 import Navbar from '../../components/Navbar';
+import './home.css';
 
 function FamiliaDados() {
-  return (
-    <div>
-      <Navbar />
-        <h3>Dados família</h3>
+    const [saldoCC, setSaldoCC] = useState('');
+    const [saldoPP, setSaldoPP] = useState('');
+    const [users, setUsers] = useState(['']);
+  
+    useEffect(() => {
+        fetch('http://localhost:5000/centralizarcontas', {
+            method: 'GET', 
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(response => response.json())
+        // .then(data => console.log(data));
+        .then(data => {
+          console.log("centralizar contas - família")
+          setUsers(data["membros"])
+          setSaldoCC(parseFloat(data["saldo_cc"]))
+          setSaldoPP(parseFloat(data["saldo_pp"]))
+        });    
+    }, []);
+
+    return (
+        <div>
+        <Navbar />
+        <h3>Dados da família</h3>   
+        <h4>Saldos totais</h4>   
+        <p>Saldo CC: R$ { saldoCC }</p>
+        <p>Saldo PP: R$ { saldoPP }</p>
+
+        <div>
+        {users.map((user, index) => (
+          <div className="user-preview" key={index} >
+            {/* <Link to={`/blogs/${account.id}`}> */}
+              <h2>{ user.nome }</h2>
+              <p>Saldo CC: R$ { user.saldo_cc }</p>
+              <p>Saldo PP: R$ { user.saldo_pp }</p>
+            {/* </Link> */}
+          </div>
+        ))}
+        </div>
+        
     </div>
-  );
+    );
 }
 
 export default FamiliaDados;
--- src/pages/my_accounts/my_home.js
+++ src/pages/my_accounts/my_home.js
@@ -1,36 +1,51 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react';
+
 import Navbar from '../../components/Navbar';
+import './my_home.css';
 
-const MeusDados = () => {
-  const [allData, setAllData] = useState('');
-  const [idFamilia, setIdFamilia] = useState('');
-  const [saldoCC, setSaldoCC] = useState(10);
-  const [saldoPP, setSaldoPP] = useState(210);
+function MeusDados() {
+    const [saldoCC, setSaldoCC] = useState('');
+    const [saldoPP, setSaldoPP] = useState('');
+    const [accounts, setAccounts] = useState(['']);
+  
+    useEffect(() => {
+        fetch('http://localhost:5000/centralizarcontas', {
+            method: 'GET', 
+            headers: { "Content-Type": "application/json" }
+        })
+        .then(response => response.json())
+        // .then(data => console.log(data));
+        .then(data => {
+          console.log("centralizar contas - user")
+          setAccounts(data["membros"][0]["contas"])
+          setSaldoCC(parseFloat(data["membros"][0]["saldo_cc"]))
+          setSaldoPP(parseFloat(data["membros"][0]["saldo_pp"]))
+        });    
+    }, []);
 
-  fetch('http://localhost:5000/centralizarcontas', {
-      method: 'GET',
-      headers: { "Content-Type": "application/json" }
-  })
-  .then(response => response.json())
-  // .then(data => console.log(data));
-  .then(data => {
-    // setAllData(data.membros.contas)
-    console.log(data.membros)
-    // setIdFamilia(data.id_familia)
-    // setSaldoCC(parseFloat(data.saldo_cc))
-    // setSaldoPP(parseFloat(data.saldo_pp))
-  });    
-
-  return (
-    <div>
+    return (
+        <div>
         <Navbar />
         <h3>Meus dados - contas</h3>   
-        
-        
+
         <p>Saldo CC: R$ { saldoCC }</p>
         <p>Saldo PP: R$ { saldoPP }</p>
+
+        <div>
+        {accounts.map((account, index) => (
+          <div className="account-preview" key={index} >
+            {/* <Link to={`/blogs/${account.id}`}> */}
+            <h2>{ account.id_banco }</h2>
+            <p>Número da conta: { account.cc }</p>
+            <p>Saldo CC: R$ { account.saldo_cc }</p>
+            <p>Saldo PP: R$ { account.saldo_pp }</p>
+            {/* </Link> */}
+          </div>
+        ))}
+        </div>
+        
     </div>
-  );
+    );
 }
-   
+
 export default MeusDados;
